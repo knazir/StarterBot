87ef9d3a4c284edd7ea7acd4ecd8b3cbdf0d205b
knazir <knazir@stanford.edu>
2018-07-09 23:37:34 -0700

    Downgrade mongodb version, add collections argument to mongodb module, bump version to 1.2.1

--- modules/MongoDb.js
+++ modules/MongoDb.js
@@ -5,29 +5,39 @@ const { MongoClient, ObjectID } = require("mongodb");
 const { Module } = require("../");
 
 module.exports = class MongoDb extends Module {
-  constructor({ databaseUrl }) {
+  constructor({ databaseUrl, collections }) {
     super();
     this._databaseUrl = databaseUrl;
+    this._collections = collections || [];
   }
 
   getKey() {
     return "mongodb";
   }
 
-  start() {
-    this._client = MongoClient.connect(this._databaseUrl, { useNewUrlParser: true });
+  async start() {
+    this._client = await MongoClient.connect(this._databaseUrl);
+    this.collections = {};
+    this._collections.forEach(collectionName => {
+      let collection = this.getCollection(collectionName);
+      if (!collection) {
+        this.createCollection(collectionName);
+        collection = this.getCollection(collectionName);
+      }
+      this.collections[collectionName] = collection;
+    });
   }
 
   //////////////// API ////////////////
 
-  addToCollection(collectionName, item) {
-    const collection = this._client.collection(collectionName);
-    if (!collection) throw new Error(`Collection ${collectionName} does not exist.`);
+  addToCollection(collection, item) {
+    collection = typeof collection === "string" ? this._client.collection(collection) : collection;
+    if (!collection) throw new Error(`Collection ${collection} does not exist.`);
     Array.isArray(item) ? collection.insertMany(item) : collection.insertOne(item);
   }
 
-  deleteFromCollection(collectionName, item) {
-    const collection = this._client.collection(collectionName);
+  deleteFromCollection(collection, item) {
+    collection = typeof collection === "string" ? this._client.collection(collectionName) : collection;
     if (!collection) throw new Error(`Collection ${collectionName} does not exist.`);
     Array.isArray(item) ? collection.deleteMany(item) : collection.deleteOne(item);
   }
